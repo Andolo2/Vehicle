2a313009b388fc2444cee0dc3b61791152db728e
Andolo2 <andreasolofsson123@gmail.com>
2022-04-07 19:53:00 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            static string Concat(string p1, string p2)
+            {
+                string value = p1 + p2;
+                return value;
+            }
+ 
+
+        }
+
+
+    }
+
+
+}
