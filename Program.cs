1dc9cbd6f9c4e6057c6f59e0cae998a39f777d63
Andolo2 <andreasolofsson123@gmail.com>
2022-04-07 19:54:37 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -6,24 +6,106 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Trucker 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Vehicle volvo = new Truck(100,"green", 40);
+            Console.WriteLine(volvo.ToString());
+            Console.WriteLine(volvo.Honk);
             
-            static string Concat(string p1, string p2)
-            {
-                string value = p1 + p2;
-                return value;
-            }
- 
+            
+            Console.ReadLine();
+            
+            
+
 
         }
+        abstract class Vehicle
+        {
+            protected int horsePower;
+            protected string color;
+
+            protected Vehicle(int _horsepower, string _color)
+
+
+            {
+                horsePower = _horsepower;
+                color = _color; 
+
+            }
+
+            public virtual int CalcSpeed()
+            {
+                
+
+                var topspeed = horsePower *2;
+
+                return topspeed;
+                
+
+            }
+            public virtual void Honk()
+            {
+                Console.WriteLine("Hooonk hoonk!");
+            }
+            
+
+
+        }
+
+        class Truck: Vehicle
+        {
+            private int load;
+            
+
+
+
+            public Truck(int _horsepower, string _color, int _load) : base(_horsepower, _color)
+            {
+                horsePower = _horsepower;
+                color = _color;
+                load = _load;
+
+            }
+
+            public override int CalcSpeed()
+            {
+                
+               var topspeed = horsePower + (load / 3) * 2;
+
+                return topspeed;
+                
+            }
+           public override string ToString()
+            {
+                string truck = string.Format(" The truck is loaded with : {0} KG.\n The color is of the truck is: {1}.\n Amount of horsepower: {2}.\n And the top speed is: {3}", load, color, horsePower, CalcSpeed());
+                return truck;
+                
+            }   
+
+
+
+        }
+
+        
+
+
+
+
 
 
     }
 
+  
 
+    
+        
+
+
+
+    
 }
